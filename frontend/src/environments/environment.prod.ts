f8a5ba1a5d92dd935a64e285301790a5db0d822a
frannivan <franciscoivansaldivar@gmail.com>
2025-12-22 01:14:11 -0600

    Fix: Point LaCanchita frontend to linking dedicated backend https://lacanchita.onrender.com

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://barbershopuat.onrender.com/api',
+    apiUrl: 'https://lacanchita.onrender.com/api',
     tenantId: 'abtech'
 };
